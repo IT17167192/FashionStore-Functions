9332de8e2930c255f091b9a4efc21898599f4799
YugmaP <40856628+YugmaP@users.noreply.github.com>
2020-03-06 20:53:35 +0530

    db name changed to fashionStore and sign up error validation

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,12 @@
 const User = require("../models/user");
+const {errorHandler} = require('../helpers/dbErrorHandler');
 
 exports.signup = (req, res) => {
     console.log('req.body', req.body)
     const user = new User(req.body);
     user.save((err, user) => {
         if(err){
-            return res.status(400).json({err});
+            return res.status(400).json({err: errorHandler(err)});
         }
 
         res.json({
