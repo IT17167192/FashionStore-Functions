da98b1d678b5c79c9d323d32ed51497d5d6d9361
YugmaP <40856628+YugmaP@users.noreply.github.com>
2020-03-08 15:15:59 +0530

    clear all unwanted console logs

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,6 @@ const {errorHandler} = require('../helpers/dbErrorHandler');
 const nodemailer = require('nodemailer');
 
 exports.signup = (req, res) => {
-    console.log('req.body', req.body)
     const user = new User(req.body);
     user.save((err, user) => {
         if(err){
