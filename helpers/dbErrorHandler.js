748a28fbd58cd71417736c1596fef3da30de9b76
YugmaP <40856628+YugmaP@users.noreply.github.com>
2020-03-10 00:20:56 +0530

    Email already exists error

--- helpers/dbErrorHandler.js
+++ helpers/dbErrorHandler.js
@@ -11,9 +11,9 @@ const uniqueMessage = error => {
             error.message.lastIndexOf("_1")
         );
         output =
-            fieldName.charAt(0).toUpperCase() +
-            fieldName.slice(1) +
-            " already exists";
+            // fieldName.charAt(0).toUpperCase() +
+            // fieldName.slice(1) +
+            "Email already exists";
     } catch (ex) {
         output = "Unique field already exists";
     }
