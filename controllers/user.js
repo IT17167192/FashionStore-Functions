0dd061533b9db6e5422b2378d6ce79090acb6fc2
Kasun Gunasekara <mails4kasun@gmail.com>
2020-03-16 21:08:05 +0530

    set used to update fields

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,10 @@
 const User = require('../models/user');
 
 exports.getUserById = (req, res, next, id) => {
-    User.findById(id).exec((err, user) => {
-        if(err || !user){
+    User.findById(id).populate('product').exec((err, user) => {
+        if (err || !user) {
             res.status(400).json({
-               error: 'User not found!'
+                error: 'User not found!'
             });
         }
 
@@ -21,8 +21,21 @@ exports.read = (req, res) => {
 };
 
 exports.update = (req, res) => {
-    User.findOneAndUpdate({_id: req.profile._id}, {$set: req.body}, {new: true}, (err, user) => {
-        if(err){
+    let updateSet = {$set: {}, $addToSet: {}};
+
+    if (req.body.name != null) {
+        updateSet.$set.name = req.body.name
+    }
+    if (req.body.password != null) {
+        updateSet.$set.password = req.body.password
+    }
+    if (req.body.email != null) {
+        updateSet.$set.email = req.body.email
+    }
+
+    console.log(updateSet);
+    User.findOneAndUpdate({_id: req.profile._id}, updateSet, {new: true}, (err, user) => {
+        if (err) {
             return res.status(400).json({
                 error: 'Unauthorized Action!'
             })
