7e245eeef3f327c1f2ccb582fff6d1149797a3aa
Kasun Gunasekara <mails4kasun@gmail.com>
2020-04-24 11:04:39 +0530

    user schema modified from scratch

--- models/user.js
+++ models/user.js
@@ -1,4 +1,49 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const uuidv1 = require('uuid/v1');
+const {ObjectId} = mongoose.Schema;
 
+const userSchema = new mongoose.Schema({
+   name: {
+       type: String,
+       trim: true,
+       required: true,
+       maxLength: 32
+   },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    hash_password: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    product: [{
+        type: ObjectId,
+        ref: 'Product',
+        required: false
+    }],
+    wishlist: [{
+        type: ObjectId,
+        ref: 'Product',
+        required: false
+    }],
+    salt: String,
+    role: {
+        type: String,
+        default: 0
+    },
+    history: {
+        type: Array,
+        default: []
+    }
+}, {timestamps: true});
 
 //virtual field
 userSchema.virtual('password')
@@ -27,3 +72,4 @@ userSchema.methods = {
     }
 };
 
+module.exports = mongoose.model("User", userSchema);
