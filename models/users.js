6cc2ff15fe04521b6509193dec54b1e9b0181ebb
YugmaP <40856628+YugmaP@users.noreply.github.com>
2020-04-24 14:29:35 +0530

    Added authentication to the project

--- models/users.js
+++ models/users.js
@@ -45,4 +45,31 @@ const userSchema = new mongoose.Schema({
     }
 }, {timestamps: true});
 
+//virtual field
+userSchema.virtual('password')
+    .set(function (password) {
+        this._password = password;
+        this.salt = uuidv1();
+        this.hash_password = this.encryptPassword(password);
+    })
+    .get(function () {
+        return this._password;
+    });
+
+userSchema.methods = {
+    auth: function(password){
+        return this.encryptPassword(password) === this.hash_password;
+    },
+    encryptPassword : function (password) {
+        if(!password) return '';
+        try{
+            return crypto.createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        }catch(err){
+            return "";
+        }
+    }
+}
+
 module.exports = mongoose.model("User", userSchema);
