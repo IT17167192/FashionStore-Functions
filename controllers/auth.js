bb12d3c56b30f4a5f960b307657d6462a3791923
YugmaP <40856628+YugmaP@users.noreply.github.com>
2020-04-25 03:18:35 +0530

    Minor changes in auth.rest and auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,7 +30,29 @@ exports.signup = (req, res) => {
                 to: req.body.email,
                 cc: process.env.EMAIL,
                 subject: 'New account details',
-                text: `Username : ${req.body.email}\nPassword : ${req.body.password}`
+                html: `<html>
+                        <head>
+                            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+                        </head>
+                        <body>
+                            <div class="newsletter-subscribe">
+                                <div class="container">
+                                    <div class="intro">
+                                    <h2 class="text-center">New Account Details</h2>
+                                    <p class="text-center">Username :&nbsp; ${req.body.email}</p>
+                                    <p class="text-center">Password :&nbsp; ${req.body.password}</p>
+                                    </div>
+                                
+                                    <button class="btn btn-primary"><a href="http://localhost:3000/signin">VISIT</a></button>
+                                </div>
+                            </div>
+                            <script src="assets/js/jquery.min.js"></script>
+                            <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+                        </body>
+                        </html>`
             };
 
             transporter.sendMail(emailOptions, function (err, success) {
